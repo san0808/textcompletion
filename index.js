ef315d6276dc18ac564e77e8329c40e8c148ec32
Sanket <sanketbhat882002@gmail.com>
2023-04-10 15:11:43 +0530

    added the suggestion to return

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001') // replace with the domain of your app
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.get('/', async (req, res) => {
   try {
     const completion = await openai.createCompletion({
@@ -17,7 +24,9 @@ app.get('/', async (req, res) => {
       prompt:
         'Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.',
     });
-    res.send(completion.data.choices[0].text);
+    suggestion=completion.data.choices[0].text;
+    res.send(suggestion);
+
   } catch (error) {
     console.log(error);
     res.status(500).send('Something went wrong!');
